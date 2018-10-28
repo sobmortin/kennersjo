c282777a001844623a716fbd91554e766a05f393
Sebastian Martin <sebastianmartin@Sebastians-MBP.lan>
2018-10-28 13:45:08 +0000

    best effort

--- challenges/index.js
+++ challenges/index.js
@@ -3,14 +3,17 @@
   
 function modulo (a, b) {
     // return the remainder of the division a / b
+    return a%b
 }
   
 function squareRoot (n) {
     // return the square root of n
+    return Math.sqrt(n)
 }
   
 function raiseToPower (m, n) {
     // return the result of raising m to the nth power
+    return Math.pow(m, n)
 }
 
 function formatMoney (amount) {
@@ -23,106 +26,199 @@ function formatMoney (amount) {
      * Amounts should always have 2 decimal numbers even if they are zeroes
      * formatMoney(1) => "£1.00"
      */
+    let currency = amount.toFixed(2)
+    return '£' + currency
 }
   
 function calculateCircleArea (r) {
     // return the area of a circle with radius r
     // round result to 3 decimal numbers
+   let area = Math.PI * (r*r)
+   let area3 = parseFloat(area.toFixed(3))
+   return area3
 }
   
 function calculateFullTurns (degrees) {
     // return the number of full turns you can make with the provided degrees
     // 1 full turn === 360 degrees
+    let turns = degrees / 360
+    return Math.floor(turns)
 }
 
 function getLength (str) {
     // return the length of the passed string
+    return str.length
 }
 
 
 function putNamesTogether (firstName, lastName) {
     // return the first and last names with a space between them
+    return firstName + ' ' + lastName
 }
   
 function capitaliseString (str) {
     // return the passed string with its first letter capitalised
+    return str[0].toUpperCase() + str.slice(1)
 }
   
 function dontShoutSentence (SENTENCE) {
     // return the passed sentence (IN ALL CAPS) in lower case
     // keep the first character capitalised
+   let lowerSentence = SENTENCE.toLowerCase()
+   return lowerSentence[0].toUpperCase() + lowerSentence.slice(1)
 }
 
 function getMiddle (str) {
     // return the middle (or middle two) character(s) of the passed string
+    let length = str.length
+    let middle = length / 2
+    if (Number.isInteger(middle)) {return str[middle] + str[middle - 1]
+    } else {
+        return str[Math.floor(middle)]
+    }
 }
-  
+
 function getLastWord (words) {
     // return the last word of a sentence
     // you can assume words doesn't have a period at the end
+   let splitWords = words.split(' ')
+   let count = -1
+   for (let i = 0; i < splitWords.length; i++) {
+       count = count + 1
+   }
+   return splitWords[count]
 }
   
 function hyphenateWords (words) {
     // return the passed string with the spaces replaced by hyphens ("-")
+   return words.split(' ').join('-')
+  
 }
   
 function convertToCamelCase (words) {
     // convert the passed string of space-separated words to camel case
     // camel case looks like this --> camelCaseLooksLikeThis
+    return words.split(' ').map(function(word,index){
+        if(index == 0){
+          return word.toLowerCase();
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }).join('');
 }
 
 function areValuesEqual (left, right) {
     // return true if the passed arguments are strictly equal
+     if (left === right) {
+         return true
+     } else {
+         return false
+     }
 }
   
 function areValuesNotEqual (left, right) {
     // return true if the passed arguments are NOT equal
+    if (left !== right) {
+        return true
+    } else {
+        return false
+    }
 }
   
 function isFromThe60s (year) {
     // return true if the year is in the 1960's
+    if ( year > 1959 && year < 1970) {
+        return true
+    } else {
+        return false
+    }
 }
   
 function isEvenLength (string) {
     // return true if string has an even length
-}
+    let length = (string.length) / 2
+    return Number.isInteger(length)
+    }
   
 function areStringsEqualCaseInsensitive (left, right) {
-    // return true if the passed strings are equal ignoring case
+    // return true if the passed strings are equal ignoring case 
+    let lowerLeft = left.toLowerCase()
+    let lowerRight = right.toLowerCase()
+    if (lowerLeft === lowerRight) {
+        return true
+    } else {
+        return false
+    }
 }
- 
   
 function getEvenNumbers (nums) {
     //return an array of only the even numbers in the given array
+    let arr = []
+    for (let i = 0; i < nums.length; i++) {
+        if (Number.isInteger(nums[i] / 2)) {
+            arr.push(nums[i])
+        }
+    }
+    return arr
 }
   
 function getItemsLongerThan (array, len) {
     //return an array of only the items longer than the given length
+    let emptyArr = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > len) {
+            emptyArr.push(array[i])
+        }
+    }
+    return emptyArr
 }
   
 function getLastItems (array, n) {
     // return an array with the 'n' last items of 'array'
+    return array.slice(Math.max(array.length - n))
 }
+
   
 function getSandwichFilling (sandwich) {
     // if an array is a sandwich, the first and last items are the bread, return an array with the filling of the sandwich
+    sandwich.pop();
+    sandwich.shift();
+    return sandwich
 }
+
   
 function removeItem (array, n) {
     // return a new array without the item on position 'n', effectively removing it from the array
+    const newArr = [...array]
+    newArr.splice(n, 1);
+    return newArr
 }
   
 function mergeArrays (arr1, arr2) {
     // return a new array with all the elements of arr1 followed by all the elements of arr2
+    return arr1.concat(arr2);
 }
-  
+
 function flattenArray (arrayOfArrays) {
     // given a nested array or arrays, return a new, flat array with all the elements of all the nested arrays in their original order
+     var flattened = arrayOfArrays.reduce(function(a, b) {
+        return a.concat(b);
+    }, []);
+    return flattened
 }
-  
+
 function isItemOmnipresent (arrayOfArrays, item) {
     // return true if the passed item is present in all the arrays inside arrayOfArrays
-}
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+       // for (let j = 0; arrayOfArrays[i].length; j++) {
+            if (arrayOfArrays[i].includes(item)) {
+                return true
+            } 
+            else {
+                return false
+            }
+        }
+    }
+
   
 function isOver40 (user) {
     /*
